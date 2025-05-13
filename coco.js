295ddd47396248f3e4b714a319f5322941969cf1
Lucaeuu <luca.skubisz@escola.pr.gov.br>
2025-05-13 10:57:57 +0000

    coco do chinese

---
+++ coco.js
@@ -0,0 +1,50 @@
+let nome = "Angel of the night"
+
+
+console.log(`Olá, meu nome é ${nome}.`);
+
+let idadeatual = 17; 
+let cidade = "Curitiba"; 
+
+console.log(`Tenho ${idadeatual} anos e moro em ${cidade}.`);
+
+let comida1 = "Pizza";
+let comida2 = "Sushi";
+let comida3 = "coco da mahara"; 
+
+console.log(`Minhas comidas favoritas são: ${comida1}, ${comida2} e ${comida3}.`);
+
+let preco =3080
+
+console.log(`O produto custa R$ ${preco}.`);
+
+let produto = "guitarra warlock"; 
+let quantidade = 1; 
+
+console.log(`Comprei ${quantidade} unidades de ${produto}.`);
+
+let temDesconto = true;
+
+console.log(`Tem desconto disponível? ${temDesconto}`);
+
+
+let anoAtual = 2025; 
+let anoNascimento = 2008; 
+
+
+let idade = anoAtual - anoNascimento;
+
+
+console.log(`Você tem ${idade} anos.`);
+
+let curso = "Psicologia"; 
+let instituicao = "Federal"; 
+
+console.log(`Estou cursando ${curso} no ${instituicao}.`);
+
+let cor1 = "preto";
+let cor2 = "vermelho";
+let cor3 = "Roxo"; 
+let cor4 = "rosa";
+
+console.log(`Minhas cores preferidas são ${cor1}, ${cor2}, ${cor3}, ${cor4}`);
